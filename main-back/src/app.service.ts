066c2664a69b306821b22e50206996786d5ff6d9
Dmitriy Sekachev <dmitriy.sekachev@softdev.pw>
2024-06-28 23:31:46 +0600

    feat: add cassandra driver

--- main-back/src/app.service.ts
+++ main-back/src/app.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { Client } from 'cassandra-driver';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  async getHello(): Promise<string> {
+    const client = new Client({
+      contactPoints: ['h1', 'h2'],
+      localDataCenter: 'datacenter1',
+      keyspace: 'ks1',
+    });
+
+    await client.execute('select * from events where created_at > ?', [
+      new Date().toISOString(),
+    ]);
     return 'Hello World!';
   }
 }
